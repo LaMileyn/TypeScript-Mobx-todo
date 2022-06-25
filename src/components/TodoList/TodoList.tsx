f8374ff048480cfe3aedd01c77e529d9d993b7e4
Volg <lost-is-died@mail.ru>
2022-06-26 02:03:14 +0500

    Переделан MySelect

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,34 +1,26 @@
-import React, {ChangeEvent, FC} from 'react';
+import React, { FC } from 'react';
 import s from './TodoList.module.scss'
 import {observer} from "mobx-react-lite";
 import Todos from "../Todos/Todos";
 import TodoStore from "../../mobx/Todos/TodoStore";
 import todoStore from "../../mobx/Todos/TodoStore";
-import {todoStatesToFilter} from "../../appTypes/TodoTypes";
+import MySelect, {IMySelectData} from "../MySelect/MySelect";
 
 
 const TodoList : FC = observer( (props) => {
 
-    const changeSelect = (e : ChangeEvent<HTMLSelectElement>) =>{
-        console.log(e.target.value)
-        todoStore.changeCurrentStateTodos(e.target.value)
+    const changeSelect = (text : string) =>{
+        todoStore.changeCurrentStateTodos(text)
     }
+    const selectData : IMySelectData[] =
+        [ { name : "Все", value : "all"}, { name: "Выполненные", value: "done"}, { name: "Не выполненные", value: "process"}]
     return (
         <section className={s.todoList}>
             <div className={s.todoList__headline}><h2>TodoList</h2></div>
             <div className={s.todoList__filters}>
                 <div className={s.filters__title}>filter:</div>
                 <div className={s.filters__select}>
-                    {/*------*/}
-
-                    <select name="select" id="sel" value = {TodoStore.currentStateTodos} onChange={ e => changeSelect(e)}>
-                        <option value="all">All</option>
-                        <option value="done">Done</option>
-                        <option value="process">In process</option>
-                    </select>
-
-                    {/* ----- */}
-
+                    <MySelect  data={selectData} onChange={changeSelect} value={TodoStore.currentStateTodos}/>
                 </div>
             </div>
             <Todos todos={TodoStore.currentTodos}/>
